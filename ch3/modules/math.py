67178b04e6d5e651d50cadb87da31f08f26e10e0
Akshay Dandekar <info@akshaydandekar.in>
2022-01-15 20:01:07 +0530

    ch3 Modules demo

---
+++ ch3/modules/math.py
@@ -0,0 +1,18 @@
+﻿PI = 3.14159
+
+def add(val1, val2):
+    return val1 + val2
+
+def sub(val1 , val2):
+    return val1 - val2
+
+
+print(__name__)
+
+if(__name__ == "__main__"):
+    var1 = 10
+    var2 = 20
+
+    sum = add(var1, var2)
+
+    print(f'Sum of {var1} and {var2} is \"{sum}\"')
---
+++ ch3/myimplementation.py
@@ -0,0 +1,12 @@
+﻿from modules.math import add
+from modules.math import sub
+
+# import mathfunctions
+
+print(__name__)
+
+var1 = 40
+var2 = 60
+
+sum = add(var1, var2)
+print(f'Sum of {var1} and {var2} is \"{sum}\"')
